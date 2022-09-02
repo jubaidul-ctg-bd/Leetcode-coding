9acbd59a3482a7ec6ba7b8bd1508d70def8e28f4
jubaidul-ctg-bd <43210438+jubaidul-ctg-bd@users.noreply.github.com>
2022-09-02 23:11:40 +0600

    Time: 128 ms (31.78%), Space: 45.3 MB (38.70%) - LeetHub

---
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function(head) {
+    let set = new Set()
+    let current = head
+    while(current) {
+        if(set.has(current)) return current 
+        set.add(current) 
+        current = current.next
+    }
+    return null
+};
