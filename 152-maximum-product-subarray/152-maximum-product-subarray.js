c2097311f225ee1954bac25acc158cfc1f5432a0
jubaidul-ctg-bd <43210438+jubaidul-ctg-bd@users.noreply.github.com>
2022-09-05 18:26:10 +0600

    Time: 116 ms (21.57%), Space: 42.4 MB (81.24%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.js
+++ 152-maximum-product-subarray/152-maximum-product-subarray.js
@@ -9,15 +9,10 @@ var maxProduct = function(nums) {
     let len=nums.length 
     let currentMax=1
     for(let i=0; i<len; i++) {
-        if(nums[i]==0) {
-            currentMin = 1
-            currentMax = 1
-        }
-        
         let tmp = currentMax*nums[i]
         currentMax = Math.max(tmp, Math.max(nums[i], currentMin*nums[i]))
         currentMin = Math.min(tmp, Math.min(nums[i], currentMin*nums[i]))
-        sum = Math.max(sum, Math.max(currentMax, nums[i]))  
+        sum = Math.max(sum, currentMax)  
     }
     return sum
 };
