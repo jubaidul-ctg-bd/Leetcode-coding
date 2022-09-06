96de010eddc8b798c37895001ed50a69c88d0fd8
jubaidul-ctg-bd <43210438+jubaidul-ctg-bd@users.noreply.github.com>
2022-09-06 17:34:26 +0600

    Time: 154 ms (31.90%), Space: 45.4 MB (49.74%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.js
@@ -0,0 +1,29 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+
+
+var dfs=(image, sr, sc, color, point)=>{
+    image[sr][sc]=color
+    if(sr>0 && image[sr-1][sc]!=color && image[sr-1][sc]==point) dfs(image, sr-1,sc,color,point)
+    if(sr<image.length-1 && image[sr+1][sc]!=color && image[sr+1][sc]==point) dfs(image, sr+1,sc,color,point)
+    if(sc>0 && image[sr][sc-1]!=color && image[sr][sc-1]==point) dfs(image, sr,sc-1,color,point)
+    if(sc<image[sr].length-1 && image[sr][sc+1]!=color && image[sr][sc+1]==point) dfs(image, sr,sc+1,color,point)
+    return image
+}
+
+var numIslands = function(grid) {
+    let cnt=0
+    for(let i=0; i<grid.length; i++) {
+        for(let j=0; j<grid[i].length; j++) {
+             if(grid[i][j]==1) {
+                cnt+=1
+                dfs(grid, i, j, 2, 1)
+            }
+        }
+    }
+        
+    return cnt
+    
+};
