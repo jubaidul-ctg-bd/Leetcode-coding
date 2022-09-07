d4b6356c39a631c38872ab647285d157346a0935
jubaidul-ctg-bd <43210438+jubaidul-ctg-bd@users.noreply.github.com>
2022-09-07 11:06:59 +0600

    Time: 92 ms (61.34%), Space: 42 MB (33.00%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n) {
+    let Fib=[0,1]
+    for(let i=2; i<=n; i++) {
+        Fib[i] = Fib[i-1]+Fib[i-2]
+    }
+    return Fib[n]
+};
