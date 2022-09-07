abed3a05d0fb8d37b1985d8fc89358754d8a6f4d
jubaidul-ctg-bd <43210438+jubaidul-ctg-bd@users.noreply.github.com>
2022-09-07 12:11:11 +0600

    Time: 155 ms (49.62%), Space: 49.4 MB (20.31%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let ans=0
+    let left=0
+    let cnt=0
+    let obj={}
+    for(let i=0; i<s.length; i++) {
+        if(obj[s[i]]) {
+            if(left<=obj[s[i]]) left=obj[s[i]]
+            obj[s[i]]=i+1
+        } else {
+            obj[s[i]]=i+1
+        }
+        ans=Math.max(1+i-left, ans)
+    }
+    ans=Math.max(s.length-left, ans)
+    return ans
+};
