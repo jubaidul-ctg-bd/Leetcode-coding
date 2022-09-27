12929eb5a40e7f271f3b1c1a84bf53d775e4f874
Md. Jubaidul Alam <43210438+jubaidul-ctg-bd@users.noreply.github.com>
2022-09-27 12:40:42 +0600

    Time: 135 ms (21.98%), Space: 43.2 MB (59.08%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.js
+++ 42-trapping-rain-water/42-trapping-rain-water.js
@@ -4,18 +4,17 @@
  */
 var trap = function(height) {
     let n = height.length 
-    let left = []
-    let right = []
-    let lm = 0
-    let rm = 0
-    let ans = 0
-    for(let i=0; i<n; i++) {
-        rm = Math.max(height[n-i-1], rm)
-        right.push(rm)    
-    }
-    for(let i=0; i<n; i++) {
-        lm = Math.max(height[i], lm)
-        ans += Math.min(lm, right[n-i-1])-height[i]
+    let left = 0
+    let right = n-1
+    let lm = height[left]
+    let rm = height[right]
+    let ans = 0 
+    while(left<right) {
+        lm = Math.max(lm, height[left])
+        if(lm>height[left]) ans+=(lm-height[left])
+        rm = Math.max(rm, height[right])
+        if(rm>height[right]) ans+=(rm-height[right]) 
+        height[left]<height[right]?left++:right--
     }
     return ans
 };
