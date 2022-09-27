0e4f02e3c4238249582dcbd362d5ba319d14cadc
Md. Jubaidul Alam <43210438+jubaidul-ctg-bd@users.noreply.github.com>
2022-09-27 12:18:46 +0600

    Time: 95 ms (72.37%), Space: 44.7 MB (40.48%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.js
+++ 42-trapping-rain-water/42-trapping-rain-water.js
@@ -10,13 +10,12 @@ var trap = function(height) {
     let rm = 0
     let ans = 0
     for(let i=0; i<n; i++) {
-        lm = Math.max(height[i], lm)
-        left.push(lm)    
         rm = Math.max(height[n-i-1], rm)
         right.push(rm)    
     }
     for(let i=0; i<n; i++) {
-        ans += Math.min(left[i], right[n-i-1])-height[i]
+        lm = Math.max(height[i], lm)
+        ans += Math.min(lm, right[n-i-1])-height[i]
     }
     return ans
 };
